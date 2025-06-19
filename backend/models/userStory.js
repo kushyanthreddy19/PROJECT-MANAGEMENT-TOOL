b168a2fe6cf61bb31db8dc243a965845b09107bc
kushyanthreddy19 <128204421+kushyanthreddy19@users.noreply.github.com>
2025-06-19 19:03:34 +0530

    Delete backend directory

--- backend/models/userStory.js
+++
@@ -1,8 +0,0 @@
-// models/userStory.js
-module.exports = (sequelize, DataTypes) => {
-  const UserStory = sequelize.define('UserStory', {
-    content: { type: DataTypes.TEXT, allowNull: false },
-    projectId: { type: DataTypes.INTEGER, allowNull: false }
-  });
-  return UserStory;
-};
--- backend/routes/userStoryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { generateUserStoriesForProject } = require('../controllers/userStoryController');
-const auth = require('../middleware/authMiddleware');
-
-const router = express.Router();
-router.use(auth);  // Protected endpoint
-router.post('/generate-user-stories', generateUserStoriesForProject);
-
-module.exports = router;
